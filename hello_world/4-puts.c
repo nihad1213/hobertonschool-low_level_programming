6453998a36ae58a8fd2fe58003bad0244ea7c65c
root <nihad.nemetli@gmail.com>
2024-02-19 08:40:36 +0000

    5th Exercise

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
 int main()
-{
-  
-  puts("\"Programming is like building a multilingual puzzle\n");
+{  
+  puts("\"Programming is like building a multilingual puzzle");
 
   return 0;
 }
