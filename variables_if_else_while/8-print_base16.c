de2a1d1181370d0fe7597bfc523d1d4ee8fc003f
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-02-20 00:30:04 +0400

    Update 8-print_base16.c

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -11,11 +11,11 @@ int main(void)
 	int number;
 	char letter;
 	
-	//print number part
+	/*print number part*/
 	for (number = 0; number < 10; number++)
 		putchar((num % 10) + '0');
 
-	//print letter part
+	/*print letter part*/
 	for (letter = 'a'; letter <= 'f'; letter++)
 		putchar(letter);
 
