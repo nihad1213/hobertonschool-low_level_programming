bcae12b2c1c84a620d035146aae35a83ed087e7e
root <nihad.nemetli@gmail.com>
2024-02-19 09:00:44 +0000

    6th Exercise

---
+++ hello_world/5-printf.c
@@ -0,0 +1,23 @@
+/*
+* File Name: 5-printf.c
+* Author: Nihad Namatli
+*/
+
+
+#include <stdio.h>
+
+/*
+ *printf() - print string
+ *return 0 - always return 0
+ *main - print string "with proper grammar, but the outcome is a piece of art,"
+*/
+
+
+
+
+int main(void)
+{
+  printf("with proper grammar, but the outcome is a piece of art,");
+  
+  return 0;
+}
