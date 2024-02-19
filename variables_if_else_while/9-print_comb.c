7e6c653c1fc5bb0c7ca748999f363653dc613712
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-02-20 00:36:09 +0400

    Update 9-print_comb.c

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -8,25 +8,19 @@
  */
 int main() 
 {
-    int i, j, k, l;
-    for (i = 0; i <= 9; i++) {
-        for (j = i + 1; j <= 9; j++) {
-            for (k = j + 1; k <= 9; k++) {
-                for (l = k + 1; l <= 9; l++) {
-                    putchar(i + '0');
-                    putchar(',');
-                    putchar(' ');
-                    putchar(j + '0');
-                    putchar(',');
-                    putchar(' ');
-                    putchar(k + '0');
-                    putchar(',');
-                    putchar(' ');
-                    putchar(l + '0');
-                    putchar('\n');
-                }
-            }
-        }
-    }
-    return 0;
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
+		
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
 }
