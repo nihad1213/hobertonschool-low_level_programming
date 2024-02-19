695306c6f164d4580514c70b0ecea4504d97418a
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-02-20 00:10:00 +0400

    Create 6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print number 0 to 10
+ * 
+ * Return: Always 0
+ */
+
+int main(void)
+{
+    int number;
+
+	  for (number = 0; number < 10; number++)
+		  putchar((number % 10) + '0');
+
+	  putchar('\n');
+
+	  return (0);
+}
