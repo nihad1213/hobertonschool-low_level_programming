c7a22de20fdc6fad5fdea70ceeb342e021f7912f
root <nihad.nemetli@gmail.com>
2024-02-19 09:15:23 +0000

    7th Exercise

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -17,8 +17,8 @@ int main(void)
 {
   printf("Size of char: %zu\n", sizeof(char));
   printf("Size of int: %zu\n", sizeof(int));
-  printf("Size of long int: %zu", sizeof(long int));
-  printf("Size of long long int: %zu", sizeof(long long int));
-  printf("Size of float: %zu", sizeof(float));
+  printf("Size of long int: %zu\n", sizeof(long int));
+  printf("Size of long long int: %zu\n", sizeof(long long int));
+  printf("Size of float: %zu\n", sizeof(float));
   return(0);
 }
