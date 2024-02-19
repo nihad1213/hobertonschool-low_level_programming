87a578713c87d553d28a0b9ab5d2327797bb1660
root <nihad.nemetli@gmail.com>
2024-02-19 09:02:50 +0000

    6th Exercise

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -11,10 +11,6 @@
  *return 0 - always return 0
  *main - print string "with proper grammar, but the outcome is a piece of art,"
 */
-
-
-
-
 int main(void)
 {
   printf("with proper grammar, but the outcome is a piece of art,\n");
