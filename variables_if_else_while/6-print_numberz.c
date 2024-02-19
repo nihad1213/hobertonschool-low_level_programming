c54b24e39de73dc00e0dccf586ebf9ffbb46c4dc
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-02-20 00:11:00 +0400

    Update 6-print_numberz.c

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -8,12 +8,12 @@
 
 int main(void)
 {
-    	  int number;
+    	int number;
 
-	  for (number = 0; number < 10; number++)
-		  putchar((number % 10) + '0');
+	for (number = 0; number < 10; number++)
+		putchar((number % 10) + '0');
 
-	  putchar('\n');
+	putchar('\n');
 
-	  return (0);
+	return (0);
 }
