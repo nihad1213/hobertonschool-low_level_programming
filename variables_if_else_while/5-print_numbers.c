d7018b5eb0e58eb71fe8d8e5d4a9715926213c1a
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-02-19 23:59:13 +0400

    Create 5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - print numbers from 0 to 10
+* return always 0
+*
+ */
+int main(void)
+{
+	int num;
+
+  for(num = 0; num <= 10; num++ )
+      printf("%d", num);
+
+  printf("\n");
+  return (0);
+}
