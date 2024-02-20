6f08ec8db32cbc551befb18289b8858b655699c4
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-02-20 10:07:48 +0400

    Update 3-islower.c

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -4,9 +4,8 @@
   * _islower - Checks for lowercase character
   * @c: The character to be checked
   *
-  * Return: 1 for lowercase character or 0 for anything else
   */
 int _islower(int c)
 {
-  return (c >= 'a' && c <= 'z');
+ return (c >= 'a' && c <= 'z');
 }
