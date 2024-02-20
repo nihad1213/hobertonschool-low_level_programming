773bf549dfdcf4e1be4f2ae645579ceb27224bd7
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-02-20 09:52:38 +0400

    Update 0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -4,7 +4,17 @@
 * Return - always 0
  */
 #include "main.h"
-int _putchar(char c)
+int main(void)
 {
-  return(printf("%c", c));
-}  
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+} 
