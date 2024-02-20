264244893624c64e4200092ade76626534e7fd68
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-02-20 10:36:25 +0400

    Update 7-print_last_digit.c

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -7,15 +7,16 @@
  */
 int print_last_digit(int n)
 {
-  int lastDigit;
-  lastDigit = n % 10;
+	int lastDigit;
 
-  if(lastDigit < 0)
-  {
-    lastDigit = -1 * lastDigit;
-  }
+	lastDigit = n % 10;
 
-  _putchar(lastDigit + '0');
-  
-  return (lastDigit);
+	if (lastDigit < 0)
+	{
+		lastDigit = lastDigit * -1;
+	}
+
+	_putchar(lastDigit + '0');
+
+	return (lastDigit);
 }
