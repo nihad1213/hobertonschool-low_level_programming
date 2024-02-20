690364a5a15ba86e34cb483786592f22fc9ea5e7
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-02-20 10:12:30 +0400

    Update 4-isalpha.c

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -5,14 +5,6 @@
  *
  * Return: Always 0.
  */
-#include "main.h"
-
-/**
-  * _islower - Checks for lowercase character
-  * @c: The character to be checked
-  *
-  * Return: lowercase
-  */
 int _islower(int c)
 {
 	return (c <= 'a' && c >= 'z');
