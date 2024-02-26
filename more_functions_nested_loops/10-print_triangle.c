29a8fd9f53747ecc81ac7c04215e749bc8a1d811
root <nihad.nemetli@gmail.com>
2024-02-26 11:20:38 +0000

    Exercise 10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_triangle - draw triangle
+ *
+ *
+ * Return: void
+ */
+
+
+void print_triangle(int size)
+{
+	int i;
+	int j;
+
+	for (i = 1; i <= size; ++i)
+	{
+		for (j = 1; j <= i; ++j)
+		{
+			_putchar(" #");
+		}
+
+		_putchar("\n");
+	}
+
+}
