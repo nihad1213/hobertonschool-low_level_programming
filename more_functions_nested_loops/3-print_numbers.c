ef630ab530c76de23153b791d468994f342690f9
root <nihad.nemetli@gmail.com>
2024-02-26 08:54:26 +0000

    Exercise 2

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_numbers - print numbers
+ * Return: always trur
+ */
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	for (i = 0; i <= 9; i++ )
+	{
+		return (i);
+	}
+
+	printf("\n");
+		
+}
