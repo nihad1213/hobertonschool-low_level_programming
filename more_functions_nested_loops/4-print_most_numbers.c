243d468913ee7bd223784fc0435dda9b4db4ed0f
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-02-26 13:25:54 +0400

    Update 4-print_most_numbers.c

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -6,12 +6,12 @@
 
 void print_most_numbers(void)
 {
-	int i;
+	char i;
 
-	for (i = 0; i <= 9; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-	if (!(i == 2 || i == 4))
-		 _putchar(i);
+	if (i != '2' || i != '4'))
+		 _putchar(i + '0');
 	}
 	_putchar('\n');
 }
