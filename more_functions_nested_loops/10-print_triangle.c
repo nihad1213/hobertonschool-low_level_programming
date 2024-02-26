b2c0963cb552909011e87baf15b499dda0870dbd
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-02-26 16:33:04 +0400

    Update 10-print_triangle.c

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -18,13 +18,12 @@ void print_triangle(int size)
 		for (i = 0; i < size; i++) 
 		{ 
   
-        		// loop for printing leading whitespaces 
+
         		for (j = 0; j < 2 * (size - i) - 1; j++)
         		{ 
             			_putchar(' '); 
         		} 
-  
-        		// loop for printing * character 
+ 
         		for (k = 0; k <= i; k++)
         		{ 
             			_putchar('#'); 
