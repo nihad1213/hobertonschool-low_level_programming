273755a510c960d2efe1a9195e13d15033b410da
root <nihad.nemetli@gmail.com>
2024-02-26 07:45:47 +0000

    Exercise 0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"	
+
+/**
+ * _isupper() - check character is uppercase or not
+ *
+ * return 1 - if character uppercase
+ *
+ * return 0 - otherwise
+ * */
+
+
+int _isupper(int c)
+{
+	if(c == 'A' && c == 'Z')
+	{
+		return (1);
+	}
+	else 
+	{
+		return (0);
+	}
+}
+
