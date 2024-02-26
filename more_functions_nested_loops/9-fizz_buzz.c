5810c0b982f1b51be3590c8948e8551744161fe0
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-02-26 14:30:20 +0400

    Update 9-fizz_buzz.c

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -6,13 +6,16 @@ int main(void)
   for (i = 0; i <= 100; i++)
   {
     if (i % 15 == 0)
-      printf(" "."FizzBuzz");
+      printf("FizzBuzz");
     else if (i % 3 == 0)
       printf(" Fizz");
     else if (i % 5 == 0)
       printf(" Buzz");
     else
       printf(" %d", i);
+  /*make tab between numbers*/
+    if (i < 100)
+      printf(" ");
   }
 
   printf("\n");
