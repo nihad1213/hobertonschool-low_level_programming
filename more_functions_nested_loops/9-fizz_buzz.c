812ebb67100c88f3ede150b30e48acebc596c45f
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-02-26 14:54:50 +0400

    Update 9-fizz_buzz.c

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -25,7 +25,10 @@ int main(void)
                 {
                         printf("Buzz");
                 }
-
+                else
+                {
+                        printf("%d", i);
+                }
                 if (i < 100)
                 {
                         printf(" ");
