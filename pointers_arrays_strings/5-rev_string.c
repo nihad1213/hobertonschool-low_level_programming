585fc56a4e8051509b1d4b37a6b2f985603ab9ea
root <nihad.nemetli@gmail.com>
2024-02-27 07:25:16 +0000

    Exercise 5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_rev - print string in reverse
+ * @s: string
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int length = 0;
+
+	while (length >= 0)
+	{
+		if (s[length] == '\0')
+			break;
+		length++;
+	}
+
+	for (length--; length >= 0; length--)
+	{
+		_putchar(s[length]);
+	}
+
+	_putchar('\n');
+}
