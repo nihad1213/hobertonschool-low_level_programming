f2c44bbc2d4eb24682e5e4d98ca5628cd086892f
root <nihad.nemetli@gmail.com>
2024-02-27 06:03:23 +0000

    Exercise 1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps value of two integer
+ * @a: integer 1
+ * @b: integer 2
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	*a = b;
+	*b = a;
+}
