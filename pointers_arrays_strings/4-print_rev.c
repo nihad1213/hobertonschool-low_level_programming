48334f9ade0b164b0b98a27f3a79ade34d90a191
root <nihad.nemetli@gmail.com>
2024-02-27 07:10:21 +0000

    Exercise 4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_rev - print string in reverse
+ * @s: string
+ * Return: void
+ */
+print_rev(char *s)
+{
+	int length = 0;
+	int length2;
+
+	while (*(length + s) != '\0')
+	{
+		length++;
+	}
+
+	length = length2;
+
+	while (length >= 0)
+	{
+		if (*(length + str) != '"' && length2 != 0)
+		{
+			_putchar(*(str + length));
+		}
+	}
+
+	_putchar('\n');
+}
