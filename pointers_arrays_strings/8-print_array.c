1c3f89c7a135fbd9e14dc9f82807cae8ce34c665
root <nihad.nemetli@gmail.com>
2024-02-28 09:31:57 +0000

    Exercise 8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -12,11 +12,11 @@ void print_array(int *a, int n)
 	int i;
 	/*get size of array*/
 	size_t arraySize;
-	arraySize = sizeof(arr) / sizeof(arr[0]);
+	arraySize = sizeof(a) / sizeof(a[0]);
 
 	/*print numbers with loop*/
 	for (i = 0; i < arraySize; i++)
 	{
-		printf("%d, ", arr[i]);
+		printf("%d, ", a[i]);
 	}
 }
