3b0e6cf85f7732fd216a65770590d869b2e32ece
root <nihad.nemetli@gmail.com>
2024-02-29 09:32:41 +0000

    Exercise 9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcpy - copy string
+ * @dest: buffer
+ * @src: source
+ * Return: *dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	while (*src != '\0')
+	{
+		*src = *dest;
+		*src++;
+		*dest++;
+	}
+
+	*dest = '\0';
+
+	return *dest;
+}
