9008a01009eada546e48d3a04cc1a7213b39bc85
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-02-29 21:09:47 +0400

    Create 1-strncat.c

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _strncat - concat 2 string
+* @dest: dest string
+* @src:src string
+* @n: integer
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, j;
+
+	while (dest[i] != '\0')
+		i++;
+	for (j = 0; j < n && src[j] != '\0'; j++, i++)
+		dest[i] = src[j];
+	dest[i] = '\0';
+	return (dest);
+}
