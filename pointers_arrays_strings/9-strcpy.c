fde726c35964ea0140ffd25efd0a5ebbb5a96d1b
root <nihad.nemetli@gmail.com>
2024-02-29 09:40:56 +0000

    Exercise 9

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -9,8 +9,14 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	while ( *dest++ = *src++ )
+	int i = 0;
+	while (str[i] != '\0')
 	{
-		;
+		i++;
+		dest[i] = src[i];
 	}
+
+	dest[i] = '\0';
+
+	return (dest);
 }
