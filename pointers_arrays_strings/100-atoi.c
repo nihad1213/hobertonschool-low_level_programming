8aed0c78657fcb8fce04218111c85499727fb65b
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-02-29 14:37:53 +0400

    Update 100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,19 +8,18 @@
 int _atoi(char *s)
 {
 	int i = 0;
+	int val = 0;
+	int minus = -1;
 	
 	while(s[i] == 32 || (s[i] >= 9 && s[i] <= 13))
 	{
 		i++;
-		int minus = 1;
 
 		if (s[i] == '-')
 		{
 			minus = -1;
 			i++;
 		}
-
-		int val = 0;
 
 		while (s[i] >= 48 && s[i] <= 57)
 		{
