cbd9096cc5fc6b70276af4e21d1ce2fd478c2ad6
root <nihad.nemetli@gmail.com>
2024-02-29 09:43:04 +0000

    Exercise 9

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -4,16 +4,16 @@
  * _strcpy - copy string
  * @dest: buffer
  * @src: source
- * Return: *dest
+ * Return: dest
  */
 
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
-	while (str[i] != '\0')
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
 	{
-		i++;
-		dest[i] = src[i];
+		src[i] = dest[i];
 	}
 
 	dest[i] = '\0';
