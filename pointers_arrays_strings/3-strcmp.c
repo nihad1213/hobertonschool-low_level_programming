12e24aecf35743249ca051f5b7aa08995595b28d
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-02-29 21:20:58 +0400

    Update 3-strcmp.c

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -11,8 +11,8 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; j++)
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 		if (s1[i] != s2[i])
-			return (s1[j] - s2[j]);
+			return (s1[i] - s2[i]);
 	return (0);
 }
