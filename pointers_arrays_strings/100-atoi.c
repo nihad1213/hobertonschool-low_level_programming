f9382439276dc1205ec483da8e5f589de8d6f95b
root <nihad.nemetli@gmail.com>
2024-02-29 10:04:54 +0000

    Exercise 10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -9,10 +9,15 @@ int _atoi(char *s)
 {
 	int i;
 	int num = 0;
-
+	
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		num = num * 10 + (s[i] - '0');	
+		
+		if (num < 0)
+		{
+			num = num * (-1);
+		}
 	}
 
 	return (num);
