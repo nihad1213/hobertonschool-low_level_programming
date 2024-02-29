bc71266c651ee995b0ae49366da5486247c56d92
root <nihad.nemetli@gmail.com>
2024-02-29 09:56:36 +0000

    Exercise 10

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _atoi() - convert string to an integer
+ * @s: string
+ * Return: 0
+ */
+int -atoi(char *s)
+{
+	int i;
+	int num = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		num = num * 10 + (s[i] - 48);	
+	}
+
+}
