e5347a82798b40628e218871fe7ef1537b7debc4
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-02-29 21:17:44 +0400

    Create 2-strncpy.c

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,17 @@
+/**
+ * _strncpy - copies string from src to dest
+ * @dest: dest string
+ * @src: src string
+ * @n: integer
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	for (j = 0; j < n && src[j] != '\0'; j++)
+		dest[j] = src[j];
+	for (; j < n; j++)
+		dest[j] = '\0';
+	return (dest);
+}
