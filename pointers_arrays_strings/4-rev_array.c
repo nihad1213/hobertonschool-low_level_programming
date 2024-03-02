9a3a978d862d09450b5ed4d832bb6a2c1699030c
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-03-02 15:24:01 +0400

    Update 4-rev_array.c

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -9,9 +9,12 @@
  */
 void reverse_array(int *a, int n)
 {
-	int j, b, i = n;
-	int temp;
+	int tmp, i;
 
-	for (i--, j = 0; j < n / 2; j++, i--)
-		temp = a[j], a[j] = a[i], a[i] = b;
+	for (i = n - 1; i >= n / 2; i--)
+	{
+		tmp = a[n - 1 - i];
+		a[n - 1 - i] = a[i];
+		a[i] = tmp;
+	}
 }
