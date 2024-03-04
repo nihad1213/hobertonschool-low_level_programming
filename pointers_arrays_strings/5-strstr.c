4a5b33804e59b06f83f7b6a6bc253159869d7827
root <nihad.nemetli@gmail.com>
2024-03-04 11:15:14 +0000

    Exercise 5

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,6 +1,22 @@
 #include "main.h"
 
 /**
+ * _strcmp - compare 2 string
+ * @s1: string 1
+ * @s2: string 2
+ * Return: 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	return (0);
+}
+
+/**
  * _strstr - locates a substring
  * @haystack: string 1
  * @needle: string 2
@@ -8,14 +24,16 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	static char *ptr;
-	ptr = haystack;
-
-	while (*ptr)
+	unsigned int length;
+	lenght = 0;
+	
+	while (needle[len] != '\0')
+		length++;
+	while (*haystack != '\0')
 	{
-		if(strncmp(ptr, needle, strlen(needle)) == 0)
-			return (ptr);
-		ptr++;
+		if (_strcmp(haystack, needle, len) == 0)
+			return (haystack);
+		haystack++;
 	}
 	return (NULL);
 }
