d62227f929fc3844f12936f773ffed56de9c3a37
root <nihad.nemetli@gmail.com>
2024-03-04 09:13:48 +0000

    Exercise 2

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -5,18 +5,10 @@
  * _strchar -  locates a character in a string
  * @s: string
  * @c: character
- * Return: ptr
+ * Return: Null
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
-	{
-		if (*s == c)
-		{
-			return (char *)s;
-			s++;
-		}
-	}
-
-	return NULL;
+	while ( *str && *str != ch ) ++str;
+    	return ( char * )( ch == *str ? str : NULL ); 
 }
