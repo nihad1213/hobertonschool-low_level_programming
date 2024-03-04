19997a5588fc54dfdc17c95009e574cab966822c
root <nihad.nemetli@gmail.com>
2024-03-04 10:03:11 +0000

    Exercise 3

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,6 +1,30 @@
 #include "main.h"
 #include <string.h>
 
+#include <stddef.h>
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string
+ * @c: character
+ * Return: Null
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s && *s != c)
+	{
+		++s;
+	}
+	if (c == *s)
+	{
+		return ((char *)s);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
+
 /**
  * _strspn - get length of prefix substring
  * @s: string 1
@@ -15,7 +39,7 @@ unsigned int _strspn(char *s, char *accept)
 	{
 		return length;
 	}
-	while (*s && strchr(accept, *s++))
+	while (*s && _strchr(accept, *s++))
 	{
 		length++;
 	}
