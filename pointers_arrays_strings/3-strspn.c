a219f487552dfdc285987c11587e77a4308cbade
root <nihad.nemetli@gmail.com>
2024-03-04 09:58:16 +0000

    Exercise 3

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strspn - get length of prefix substring
+ * @s: string 1
+ * @accept: string 2
+ * Return: number of bytes
+ */
+unsigned int_strspn(char *s, char *accept)
+{
+	unsigned int length = 0;
+
+	if ((s == NULL) && (accept == NULL))
+	{
+		return length;
+	}
+	while (*s && strchr(accept, *s++))
+	{
+		length++
+	}
+
+	return (length);
+}
