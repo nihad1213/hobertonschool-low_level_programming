e84e6cfdb5a33991a072951fa724132b5b1734a6
root <nihad.nemetli@gmail.com>
2024-03-04 08:26:42 +0000

    Exercise 1

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -12,7 +12,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	/*typecast src and dest addresss to (char *) */
 	char *typeCastSrc = (char *)src;
 	char *typeCastDest = (char *)dest;
-	int i = 0;
+	unsigned int i = 0;
 
 	for (i = 0; i < n; i++)
 	{
