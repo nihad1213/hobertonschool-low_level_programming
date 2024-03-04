94ce328cfe7b7c11dd51f91078ecee1c66f0cf95
root <nihad.nemetli@gmail.com>
2024-03-04 11:30:19 +0000

    Exercise 5

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -4,9 +4,37 @@
  * _strcmp - compare 2 string
  * @s1: string 1
  * @s2: string 2
- * Return: 0
+ * Return: NULL
  */
 int _strcmp(char *s1, char *s2)
 {
+	char *string1, *string2;
 
+	while (*s1 != NULL)
+	{
+		if (*s1 == *s2)
+		{
+			string1 = s1;
+			string2 = s2;
+
+			while (*string1 && *string2)
+			{
+				if (*string1 != *string2)
+				{
+					break;
+				}
+
+				string1++;
+				string2++;
+				
+			}
+
+			if (*string2 == NULL)
+				return (s1);
+		}
+
+		s1++;
+	}
+
+	return (NULL);
 }
