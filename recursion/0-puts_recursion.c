c0e4e41744c3916bd1c52d6088bdf90a28415987
root <nihad.nemetli@gmail.com>
2024-03-05 06:14:12 +0000

    Task 0

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -6,13 +6,11 @@
  */
 void _puts_recursion(char *s)
 {
-	if (s[0] != '0')
-	{
-		_putchar(s[0]);
-		_puts_recursion((s + 1));
-	}
-	else
+	if (!*s)
 	{
 		_putchar('\n');
+		return;
 	}
+	_putchar(*s);
+	_puts_recursion((s + 1));
 }
