847c6dbf69e9b411a2ce829cf0fbc6d62ab358d9
root <nihad.nemetli@gmail.com>
2024-03-05 09:34:13 +0000

    Task 6

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * is_prime_number - return 1 if it is prime number
+ * otherwise 0
+ * @n: integer number
+ * Return: return 1 if it is prime number
+ * otherwise 0 
+ */
+int is_prime_number(int n)
+{
+	if (n / n == 1 && n / 1 == n)
+	{
+		return (1);
+	}
+	else if (n / is_prime_number(n - 1) == 1)
+	{
+		return (0);
+	}
+}
