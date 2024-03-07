34de3b48af51c60f930ccf3d6c99b69b5dd3e24f
root <nihad.nemetli@gmail.com>
2024-03-07 10:49:42 +0000

    Task 3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -10,17 +10,5 @@
  */
 int main(int argc, char *argv[])
 {
-	int result;
 
-	if (argc < 2)
-
-	{
-		printf("Error\n");
-		return (1);
-	}
-
-	result = (int) (*argv[1] * *argv[2]);
-	printf("%d\n", result);
-
-	return(0);
 }
