6a5ce18b1fe09a7ea4418cf5a7fc061504a358c6
root <nihad.nemetli@gmail.com>
2024-03-07 10:45:36 +0000

    Task 3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -8,11 +8,12 @@
  * Return: 1 if there is no 2 argument
  * otherwise return 1
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
 	int result;
 
 	if (argc < 2)
+
 	{
 		printf("Error\n");
 		return (1);
