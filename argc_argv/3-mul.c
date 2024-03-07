58650eb8266d40469b5c3dadff37ca3873e8daae
root <nihad.nemetli@gmail.com>
2024-03-07 11:06:55 +0000

    Task 3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -12,9 +12,8 @@ int main(int argc, char *argv[])
 {
 	int result = 1;
 	int i;
-	int lastResult;
-
-	if (argc == 1)
+	
+	if (argc == 1 || argc == 2)
 	{
 		printf("Error\n");
 		return (1);
@@ -23,7 +22,7 @@ int main(int argc, char *argv[])
 	{
 		for(i = 1; i < 3; i++)
 		{
-			lastResult = result * atoi(argv[i]); 
+			result = result * atoi(argv[i]); 
 		}
 
 		printf("%d\n", result);
