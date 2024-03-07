abab207cfd551c981dc965783d6e09fd7ad1a364
root <nihad.nemetli@gmail.com>
2024-03-07 09:48:02 +0000

    Task 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints number of arguments passed into it
+ * @argc: store number of arguments
+ * @argv: listing all arguments
+ * Return: 0
+ */
+int main(argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
