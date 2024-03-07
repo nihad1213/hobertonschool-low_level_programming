fa814d292b7cc5d15c1ee203cb18ea2b21a9d551
root <nihad.nemetli@gmail.com>
2024-03-07 10:25:47 +0000

    Task 3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - multiplirs 2 number
+ * @argc: number of arguments
+ * @argv: list arguments
+ * Return: 1 if there is no 2 argument
+ * otherwise return 1
+ */
+int main(int argc, char *argv[])
+{
+	int result;
+
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	result = atoi(argv[0]) * atoi(argv[1]);
+	printf("%d\n", result);
+}
