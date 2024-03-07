77a6d856ad5a404c7995c16e9cb92c0eee76137b
root <nihad.nemetli@gmail.com>
2024-03-07 10:00:28 +0000

    Task 2

---
+++ argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print all argument it receives 
+ * @argc: number of arguments
+ * @argv: lsiting all arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for(i = 0; i <= argc; i++)
+	{
+		printf("%s", argv[i]);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
