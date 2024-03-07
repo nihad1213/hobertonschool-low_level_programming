cfd3e02bed14c7315f3b522835ace9440703ec21
root <nihad.nemetli@gmail.com>
2024-03-07 10:07:59 +0000

    Task 2

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - print all argument it receives 
+ * main - print all argument it receives
  * @argc: number of arguments
  * @argv: lsiting all arguments
  * Return: 0
