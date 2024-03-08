4d54ac3c5c2b6a9b53e4ef3dd24152ef2c11e45c
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-03-08 14:52:00 +0400

    Update 6-is_prime_number.c

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -6,19 +6,24 @@
  * Return: return 1 if it is prime number
  * otherwise 0 
  */
+int prime_finder(int x, int y)
+{
+	if (x == y)
+		return (1);
+	if (!(x % y))
+		return (0);
+	return (prime_finder(x, y + 1));
+}
+/**
+ * is_prime_number - returns if a number is prime
+ *
+ * @n: number to determine primeness of
+ *
+ * Return: 1 if prime, 0 otherwise
+ */
 int is_prime_number(int n)
 {
-	
-	if (n / n == 1 && n / 1 == n)
-	{
-		return (1);
-	}
-	else if (n / is_prime_number(n - 1) == 1)
-	{
+	if (n < 2)
 		return (0);
-	}
-	else if (n == 1)
-	{
-		return (0);
-	}
+	return (prime_finder(n, 2));
 }
