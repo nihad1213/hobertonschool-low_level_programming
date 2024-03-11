f526420abdbe1548069a92b8217f4f97a47b01eb
root <nihad.nemetli@gmail.com>
2024-03-11 11:11:59 +0000

    Task 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -39,7 +39,5 @@ char *str_concat(char *s1, char *s2)
 	for (p = s1; (*q = *p) != '\0'; ++p, ++q) {}
   	for (p = s2; (*q = *p) != '\0'; ++p, ++q) {}
   	
-	free(p);
-	free(q);
 	return (result);	
 }
