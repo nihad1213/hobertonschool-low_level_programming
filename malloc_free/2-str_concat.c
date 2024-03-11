fa056f24aa2a36b12889194946928997e4f57359
root <nihad.nemetli@gmail.com>
2024-03-11 10:59:51 +0000

    Task 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -26,12 +26,14 @@ int _strlen(char *s)
  */
 char *str_concat(char *s1, char *s2)
 {
+
+	char *p, *q, *result;
+	
 	if (s1 == 0)
 		s1 = " ";
 	if (s2 == 0)
 		s2 = " ";
-
-	char *p, *q, *result;
+	
 	result = q = malloc(_strlen(s1) + _strlen(s2) + 1);
   	for (p = s1; (*q = *p) != '\0'; ++p, ++q) {}
   	for (p = s2; (*q = *p) != '\0'; ++p, ++q) {}
