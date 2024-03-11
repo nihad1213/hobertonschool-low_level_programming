f8f3514b75a2e68ac34aaafa3a995f14ee932fa3
root <nihad.nemetli@gmail.com>
2024-03-11 11:31:59 +0000

    task

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * alloc_grid - return a pointer to a 2
+ * dimensional arrays of integers
+ * @width: width
+ * @height: height
+ * Return: NULL on failure
+ */
+int **alloc_grid(int width, int height)
+{
+		
+}
