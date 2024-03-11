b6ff10942362dbf6a5c228ebee8f159e2a286fb7
root <nihad.nemetli@gmail.com>
2024-03-11 12:37:46 +0000

    Task 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,43 +1,31 @@
 #include "main.h"
 #include <stdlib.h>
 
-/**
- * _strlen - return length of string
- * @s: string
- * Return: void
- */
-int _strlen(char *s)
-{
-	int length = 0;
-
-	while (*(length + s) != '\0')
-	{
-		length++;
-	}
-
-	return (length);
-}
-
-/**
- * str_concat - concatenates 2 string
- * @s1: string 1
- * @s2: string 2
- * Return: NULL on failure
- */
 char *str_concat(char *s1, char *s2)
 {
-
-	char *p, *q, *result;
+	int a, b, c, d;
+	char *s;
 	
 	if (s1 == 0)
 		s1 = "";
 	if (s2 == 0)
 		s2 = "";
+
+	for (a = 0; s1[a] != '\0'; a++)
+	{
+		;
+	}
+
+	for (b = 0; s2[b] != '\0'; b++)
+	{
+		;
+	}
+
+	b++;
+
+	s = malloc(a * s1 + b * s2);
+
+	if (s == NULL)
+		return (NULL);
 	
-	result = q = malloc(_strlen(s1) + _strlen(s2) + 1);
-  	
-	for (p = s1; (*q = *p) != '\0'; ++p, ++q) {}
-  	for (p = s2; (*q = *p) != '\0'; ++p, ++q) {}
-  	
-	return (result);	
 }
