c40daa550a5a64e450cc8a476c04626ec4fd855a
root <nihad.nemetli@gmail.com>
2024-03-12 05:36:14 +0000

    Task 4

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * free_grid - frees 2 dimensional grid
+ * @grid: grid
+ * @height: height
+ * Return: Void
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+
+}
