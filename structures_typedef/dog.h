26b11f59238dc113810e8f8d9f6fcb221c0dbf8b
root <nihad.nemetli@gmail.com>
2024-03-12 07:54:49 +0000

    Task 0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - information about dog  
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} my_dog;
+
+
