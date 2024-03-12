c4171327b4a2c24a88148af0c0e70f0bb85acb26
root <nihad.nemetli@gmail.com>
2024-03-12 06:07:14 +0000

    Task 0

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - that allocate memory
+ * @usingned int b: integer
+ * Return: Void
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *pointer;
+	pointer = malloc(b);
+
+	if (pointer = NULL)
+	{
+		exit (98);
+	}
+}
---
+++ more_malloc_free/README.md
@@ -0,0 +1,1 @@
+# C programming tasks about malloc() free() exit()
