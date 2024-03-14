4ccefff8c931fa3fe2af4a0b67956da43e80ec56
root <nihad.nemetli@gmail.com>
2024-03-14 09:39:09 +0000

    Task 1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -23,7 +23,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		a++;		
 	}
 
-	s = malloc(sizeof(*a) * a + n + 1);
+	s = malloc(sizeof(*s) * a + n + 1);
 
 	if (s == NULL)
 		return (NULL);
