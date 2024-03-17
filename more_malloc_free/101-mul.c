f2733c408aa26801e06204f2a2b6bf602b55dbfe
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-03-17 14:58:52 +0400

    Create 101-mul.c

---
+++ more_malloc_free/101-mul.c
@@ -0,0 +1,63 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * multiply - Function to multiply two integers
+ * @num1: First integer
+ * @num2: Second integer
+ * Return: Result of multiplication
+ */
+int multiply(int num1, int num2)
+{
+    return num1 * num2;
+}
+
+/**
+ * is_valid_input - Function to check if input strings are valid integers
+ * @num1: First string
+ * @num2: Second string
+ * Return: 1 if valid, 0 if not
+ */
+int is_valid_input(const char *num1, const char *num2)
+{
+    while (*num1 != '\0')
+    {
+        if (!isdigit(*num1))
+            return 0;
+        num1++;
+    }
+    while (*num2 != '\0')
+    {
+        if (!isdigit(*num2))
+            return 0;
+        num2++;
+    }
+    return 1;
+}
+
+/**
+ * main - Entry point of the program
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line arguments
+ * Return: 0 on success, 98 on failure
+ */
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        printf("Error\n");
+        return 98;
+    }
+
+    if (!is_valid_input(argv[1], argv[2]))
+    {
+        printf("Error\n");
+        return 98;
+    }
+
+    int num1 = atoi(argv[1]);
+    int num2 = atoi(argv[2]);
+    int result = multiply(num1, num2);
+    printf("%d\n", result);
+
+    return 0;
+}
