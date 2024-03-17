1aa613b49724bb3fecaa1a2c9eb4748c11c8bca9
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-03-17 15:04:22 +0400

    Update 101-mul.c

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -1,22 +1,10 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * multiply - Function to multiply two integers
- * @num1: First integer
- * @num2: Second integer
- * Return: Result of multiplication
- */
-int multiply(int num1, int num2)
-{
-    return num1 * num2;
-}
-
-/**
- * is_valid_input - Function to check if input strings are valid integers
- * @num1: First string
- * @num2: Second string
- * Return: 1 if valid, 0 if not
- */
+  * _isdigit - tells if the string consists of digits
+  * @argv: pointer to current item in argument
+  * Return: return 0 if all digits, 1 if not all digits.
+  */
 int _isdigit(char *argv)
 {
 	int i;
@@ -31,48 +19,50 @@ int _isdigit(char *argv)
 	}
 	return (0);
 }
-
-int is_valid_input(const char *num1, const char *num2)
-{
-    while (*num1 != '\0')
-    {
-        if (!_isdigit(int*(num1)))
-            return 0;
-        num1++;
-    }
-    while (*num2 != '\0')
-    {
-        if (!_isdigit(*num2))
-            return 0;
-        num2++;
-    }
-    return 1;
-}
-
 /**
- * main - Entry point of the program
- * @argc: Number of command-line arguments
- * @argv: Array of command-line arguments
- * Return: 0 on success, 98 on failure
- */
+  * _atoi - converts a string of ascii digits to the values they represent
+  * @s: pointer to the source string
+  * Return: value of digits
+  */
+int _atoi(char *s)
+{
+	int i, result;
+
+	i = result = 0;
+	while (s[i])
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			result *= 10;
+			result += (s[i] - '0');
+		}
+		i++;
+	}
+	return (result);
+}
+/**
+  * main - main function call
+  * @argc: argument count
+  * @argv: 2D array of arguments
+  * Return: return 0 on success, 98 on failure
+  */
 int main(int argc, char *argv[])
 {
-    if (argc != 3)
-    {
-        printf("Error\n");
-        return 98;
-    }
+	int i;
 
-    if (!is_valid_input(argv[1], argv[2]))
-    {
-        printf("Error\n");
-        return 98;
-    }
-
-    int num1 = atoi(argv[1]);
-    int num2 = atoi(argv[2]);
-    int result = multiply(num1, num2);
-    printf("%d\n", result);
-
-    return 0;
+	malloc();
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		if (_isdigit(argv[i]))
+		{
+			printf("Error\n");
+			exit(98);
+		}
+	}
+	return (0);
 }
