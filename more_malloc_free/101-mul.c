968eab951725a7c565ebd591cc110c6ca91c88d2
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-03-17 15:07:28 +0400

    Update 101-mul.c

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -9,8 +9,10 @@ int main(int argc, char *argv[])
 unsigned long mul;
 int i, j;
 	if (argc != 3)
-	{ printf("Error\n");
-	exit(98); }
+	{
+	printf("Error\n");
+	exit(98);
+	}
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
