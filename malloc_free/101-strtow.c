4530a1193aae483e9bff742ca4160b8149154175
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-03-17 15:16:10 +0400

    Update 101-strtow.c

--- malloc_free/101-strtow.c
+++ malloc_free/101-strtow.c
@@ -1,44 +1,73 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+/**
+ * cwords - count number of words in a given string.
+ * @str: given string.
+ * @l: lenght of string.
+ * Return: number of words.
+ */
+int cwords(char *str, int l)
+{
+	int i, count_w = 0;
+
+	for (i = 0; i < l; i++)
+	{
+		if (str[i] != ' ')
+		{
+			for (; str[i] != ' '; i++)
+				;
+			count_w++;
+		}
+	}
+	return (count_w);
+}
 
 /**
-* **strtow - function that splits a string into words.
-* @str : pointer
-* Return: Char
-*/
-
+ * strtow - splits a string into words.
+ * @str: input string.
+ * Return: Pointer to array.
+ */
 char **strtow(char *str)
 {
-	char **d;
-	int i;
-	int j = 0;
-	int con = 0;
+	char **p;
+	int count_w = 0, count_c = 0, i, leng, x = 0, fr, j = 0, z = 0;
 
-	if (str == NULL)
-		return (NULL);
-
-	for (i = 0 ; str[i] != '\0' ; i++)
+	if (str == NULL || *str == '\0')
 	{
-		if (str[i] != 32)
-			con++;
-	}
-
-	d = malloc(sizeof(char) * con);
-
-	if (d == NULL)
 		return (NULL);
-
-	for (i = 0 ; str[i] != '\0' ; i++)
+	}
+	for (leng = 0; str[leng] != '\0'; leng++)
+		;
+	count_w = cwords(str, leng);
+	p = (char **)malloc((count_w + 1) * sizeof(char *));
+	if (p == NULL || count_w == 0)
 	{
-		if (str[i] != 32)
-		{
-			*d[j] = str[i];
-			j++;
-		}
-		else
-		{
+		free(p);
+		return (NULL);
+	}
+	for (i = 0; i < leng; i++)
+	{
+		if (str[i] != ' ')
+		{z = 1;
+			for (count_c = 0; str[i] != ' ' && str[i] != '\0'; i++)
+				count_c++;
+			p[x] = (char *)malloc((count_c + 1) * sizeof(char));
+			if (p[x] == NULL)
+			{
+				for (fr = 0; fr <= x; fr++)
+					free(p[fr]);
+				free(p);
+				return (NULL);
+			}
+			for (j = 0; j < count_c; j++)
+				p[x][j] = str[(i - count_c) + j];
+			p[x][j] = '\0';
+			x++;
 		}
 	}
-	return (d);
+	if (z != 1)
+		return (NULL);
+	p[x] = NULL;
+	return (p);
 }
