ae691b64dd599154f454f14b610d092f717e62c7
root <nihad.nemetli@gmail.com>
2024-03-18 08:16:52 +0000

    Task 0

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * print_name: print name
+ * @name: name
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	*name = f();
+}
