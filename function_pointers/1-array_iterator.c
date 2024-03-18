78812a8cb4eb0671b80de9ca0ee51448915b3f42
root <nihad.nemetli@gmail.com>
2024-03-18 09:07:40 +0000

    Task 1

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,11 +1,11 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator: executes function given as a paramenter
+ * array_iterator - executes function given as a paramenter
  * on each element of an array
  * @array: array
  * @size: size
- * action: a pointer to the size of the array
+ * @action: a pointer to the size of the array
  * Return: void
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
