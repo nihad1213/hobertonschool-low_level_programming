9f28a6d80c22921a10919fc0fa961d30438a837c
root <nihad.nemetli@gmail.com>
2024-03-18 09:34:30 +0000

    Task 2

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -12,7 +12,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size == NULL || cmp == NULL || array == NULL)
+	if (size < 1 || cmp == NULL || array == NULL)
 	{
 		return (-1);
 	}
