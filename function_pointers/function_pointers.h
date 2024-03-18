12d44498511b43da990319fbf1a786bda4255713
root <nihad.nemetli@gmail.com>
2024-03-18 08:08:19 +0000

    Header FILE

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef FUNCITON POINTERS_H
-#define FUNCTION POINTERS_H
+#ifndef FUNCITON_POINTERS_H
+#define FUNCTION_POINTERS_H
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 #endif
