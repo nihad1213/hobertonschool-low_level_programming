617def1b1b14431ce9a5616c20277bef326fcc11
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-03-19 11:00:12 +0400

    Update 3-main.c

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -11,31 +11,31 @@
  */
 int main(int argc, char *argv[])
 {
-       int a, b;
-       int (*o)(int, int);
+    int a, b;
+    int (*o)(int, int);
+       
+    if (argc != 4)
+    {
+        printf("Error\n");
+        exit(98);
+    }
 
-       if (argc != 4)
-       {
-                printf("Error\n");
-                exit(98);
-        }
+    if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
+    {
+       printf("Error\n");
+       exit(99);
+    }
 
-        if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
-        {
-                printf("Error\n");
-                exit(99);
-        }
-
-        if ((*argv[2] == '%' || *argv[2] == '/') && (*argv[3] == 0))
-        {
-                printf("Error\n");
-                exit(100);
-        }
+    if ((*argv[2] == '%' || *argv[2] == '/') && (*argv[3] == 0))
+    {
+        printf("Error\n");
+        exit(100);
+    }
         
-        a = atoi(argv[1]);
-        b = atoi(argv[3]);
-        o = get_op_func(argv[2]);
-        printf("%d\n", o(a, b));
-
-        return (0);
+    a = atoi(argv[1]);
+    b = atoi(argv[3]);
+    o = get_op_func(argv[2]);
+    printf("%d\n", o(a, b));
+    
+    return (0);
 }
