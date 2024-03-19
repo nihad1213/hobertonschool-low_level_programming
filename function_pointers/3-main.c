404d64cae7c8f5a418221f16bb304354df79d2ca
root <nihad.nemetli@gmail.com>
2024-03-19 08:32:39 +0000

    Task 3

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -19,20 +19,24 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		exit(98);
 	}
+	
 	if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
+	
 	if ((*argv[2] == '%' || *argv[2] == '/') && (*argv[3] == '0'))
 	{
 		printf("Error\n");
 		exit(100);
 	}
+	
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 	fp = get_op_func(argv[2]);
 	ans = fp(a, b);
 	printf("%d\n", ans);
+
 	return (0);
 }
