f0829fdb74bb739d76f04a090061ffc56d09b12b
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-03-19 10:40:15 +0400

    Update 3-main.c

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -16,19 +16,19 @@ int main(int argc, char *argv[])
 
         if (argc != 4)
         {
-                printf("ERROR\n");
+                printf("Error\n");
                 exit(98);
         }
 
         if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
         {
-                printf("ERROR\n");
+                printf("Error\n");
                 exit(99);
         }
 
         if ((*argv[2] == '%' || *argv[2] == '/') && (*argv[3] == 0))
         {
-                printf("ERROR\n");
+                printf("Error\n");
                 exit(100);
         }
         
