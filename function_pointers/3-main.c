3d72744c09f2d336e231bae8d66156ead2ba0834
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-03-19 11:21:24 +0400

    Update 3-main.c

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -9,21 +9,21 @@
  * @argv: list of arguments
  * Return: Always 0
  */
-int main(int argc, char *argv[])
+int main()
 {
-    int a, b; 
+    int a, b;
     int (*o)(int, int);
-   
+
     if (argc != 4)
     {
         printf("Error\n");
         exit(98);
     }
 
-    if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
+    if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0'))
     {
-       printf("Error\n");
-       exit(99);
+        printf("Error\n");
+        exit(99);
     }
 
     if ((*argv[2] == '%' || *argv[2] == '/') && (*argv[3] == 0))
@@ -31,11 +31,12 @@ int main(int argc, char *argv[])
         printf("Error\n");
         exit(100);
     }
-        
+
     a = atoi(argv[1]);
     b = atoi(argv[3]);
     o = get_op_func(argv[2]);
+
     printf("%d\n", o(a, b));
-    
+
     return (0);
 }
