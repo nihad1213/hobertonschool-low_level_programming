87c463ed214bb8ecd173997e30f71ebab06541b4
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-03-21 13:43:58 +0400

    Update 3-main.c

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,41 +1,41 @@
-#include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "3-calc.h"
 /**
- * main - calc
- * @argc: number of arguments
- * @argv: list of arguments
- * Return: Always 0
+ * main - entry point for function
+ * @argc: arg counter, number of args
+ * @argv: array of pointers to args
+ *
+ * Description: calculates basic arithmetic
+ *
+ * Return: 0 on success, 98 on failure
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int a, b, ans;
-	int (*fp)(int, int);
+	char op;
+	int (*op_func)(int, int);
+	int result;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
-		exit(98);
+		return (98);
 	}
-
-	if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
-	{
+	op = *(argv[2]);
+	if ((op == '/' || op == '%') && !atoi(argv[3]))
+	{ /* divide by zero exception */
 		printf("Error\n");
-		exit(99);
+		return (100);
 	}
 
-	if ((*argv[2] == '%' || *argv[2] == '/') && (*argv[3] == '0'))
-	{
+	/* actually do the operation now that we've error chcked */
+	op_func = get_op_func(&op);
+	if (op_func == NULL || argv[2][1] != '\0')
+	{ /* didn't find operator, or operator longer than 1 byte */
 		printf("Error\n");
-		exit(100);
+		return (99);
 	}
-
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-	fp = get_op_func(argv[2]);
-	ans = fp(a, b);
-	printf("%d\n", ans);
-
+	result = op_func(atoi(argv[1]), atoi(argv[3]));
+	printf("%d\n", result);
 	return (0);
 }
