5002fe92e2f6633fd4ab3c3eb02fecb2264bc30b
root <nihad.nemetli@gmail.com>
2024-04-01 08:38:43 +0000

    Task 0

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * print_lists - print all elements of list
+ * @h: list
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			_putchar('[');
+			_putchar('0');
+			_putchar(']');
+			_putchar(' ');
+			_putchar('(');
+			_puthcar('n');
+			_putchar('i');
+			_putchar('l');
+			_putchar(')');
+		}
+		else
+		{
+			printf("[%d] %s", h->len, h->str);
+		}
+
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
+}
