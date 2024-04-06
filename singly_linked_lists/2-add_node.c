f7697762b75d22f2b7da90ce3a335d03a0ab2d7a
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-04-06 15:41:49 +0400

    Create 2-add_node.c

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * add_node - Add node to the list
+ * @head: head of list
+ * @str: str to new
+ * Return: pointer to added structure
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	unsigned int len = 0;
+
+	new = malloc(sizeof(list_t));
+	if (new)
+	{
+		new->str = strdup(str);
+		if (new->str)
+		{
+			while (new->str[len])
+				len++;
+			new->len = len;
+			new->next = *head;
+			*head = new;
+			return (new);
+		}
+		free(new);
+		return (NULL);
+	}
+	return (NULL);
+}
